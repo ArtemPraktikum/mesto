aad06151ccd6206456adfe658c8b0196407e8d4f
Artem Velmozhin <artemvrabota@yandex.ru>
2021-07-16 23:17:56 +0400

    feat: сделать фул webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 module.exports = {
@@ -27,7 +28,25 @@ module.exports = {
         use: 'babel-loader',
         exclude: '/node_modules/'
       },
-      ],
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options:
+            {
+              importLoaders: 1
+            },
+          },
+          'postcss-loader',
+        ],
+      },
+    ],
   },
 
   plugins: [
@@ -35,5 +54,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './index.html'
     }),
+    new MiniCssExtractPlugin(),
   ],
 };
