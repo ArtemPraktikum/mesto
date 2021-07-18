ad2d89bdbd57515fb8f2eb79e0925c907369a59d
Artem Velmozhin <artemvrabota@yandex.ru>
2021-07-18 19:38:06 +0400

    feat: перенести mode development из webpack в pacage

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,13 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: { main: './src/pages/index.js' },
+  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
         publicPath: ''
   },
 
-  mode: 'development',
   devServer: {
     contentBase: path.resolve(__dirname, './dist'),
     compress: true,
